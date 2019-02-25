b4fa2526501dbeb517b0dc5549ddc7e1bfda3c40
Optic969 <optic969@tut.by>
2019-02-25 17:16:45 +0300

    Преременные

--- src/main/java/MyFirstProgramm.java
+++ src/main/java/MyFirstProgramm.java
@@ -1,5 +1,22 @@
 public class MyFirstProgramm {
     public static void main (String[] args){
         System.out.println("Hello World!!!!");
+        System.out.println(2+2);
+       // Пример экранирования // System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+        System.out.println(1/2);
+        /*Числа с плавающей строкой*/
+        System.out.println(2.0/5);
+        /*Конкатенация*/
+        System.out.println("Мой рост: " + 174 + " см");
+        /*Работа с переменными*/
+        int f = 2;
+        double a = 10.22;
+        double b = 20;
+        String c = "Площадь комнаты ";
+        String d = "Метров кв";
+        System.out.println( c+(a*b)+" "+d+f);
+
     }
 }
