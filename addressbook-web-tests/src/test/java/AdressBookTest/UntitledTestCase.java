5b68e1df0b30f6817f3618a687a0165db6d1e7b1
Optic969 <optic969@tut.by>
2019-03-13 13:57:37 +0300

    1 Test WORK!!!

--- addressbook-web-tests/src/test/java/AdressBookTest/UntitledTestCase.java
+++ addressbook-web-tests/src/test/java/AdressBookTest/UntitledTestCase.java
@@ -1,12 +1,13 @@
 package AdressBookTest;
 
+
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+
 
 public class UntitledTestCase {
     private WebDriver driver;
@@ -16,55 +17,22 @@ public class UntitledTestCase {
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\node_modules\\appium\\node_modules\\appium-chromedriver\\chromedriver\\win\\chromedriver.exe");
+                driver = new ChromeDriver();
         baseUrl = "https://www.katalon.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
     public void testUntitledTestCase() throws Exception {
-        driver.get("http://localhost/addressbook/");
+        driver.get("http://localhost/addressbook/group.php");
         driver.findElement(By.name("user")).clear();
         driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
         driver.findElement(By.name("pass")).clear();
         driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='PASSWORD:'])[1]/following::input[2]")).click();
-        driver.findElement(By.linkText("GROUPS")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("Test4");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("Test5");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("Test6");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
-        driver.findElement(By.linkText("GROUPS")).click();
-        driver.findElement(By.linkText("LOGOUT")).click();
         driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
         driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='PASSWORD:'])[1]/following::input[2]")).click();
-        driver.findElement(By.linkText("GROUPS")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("Test22");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("Test33");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("Test44");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
     }
 
     @AfterClass(alwaysRun = true)
