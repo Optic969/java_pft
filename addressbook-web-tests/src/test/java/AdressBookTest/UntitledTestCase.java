1869beb0f1f92c52e938aa8e7319bf628aea6da7
Optic969 <optic969@tut.by>
2019-03-20 17:55:33 +0300

    Up

--- addressbook-web-tests/src/test/java/AdressBookTest/UntitledTestCase.java
+++ addressbook-web-tests/src/test/java/AdressBookTest/UntitledTestCase.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.*;
 
 public class UntitledTestCase {
     private WebDriver driver;
-    private String baseUrl;
+    //private String baseUrl;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
@@ -19,20 +19,34 @@ public class UntitledTestCase {
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\node_modules\\appium\\node_modules\\appium-chromedriver\\chromedriver\\win\\chromedriver.exe");
         driver = new ChromeDriver();
-        baseUrl = "https://www.katalon.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://localhost/addressbook/group.php");
+        // driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        // driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        // driver.findElement(By.name("user")).click();
+        //driver.findElement(By.name("pass")).click();
+        driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
+       // baseUrl = "https://www.katalon.com/";
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
     public void testUntitledTestCase() throws Exception {
-        driver.get("http://localhost/addressbook/group.php");
+       // driver.get("http://localhost/addressbook/group.php");
        // driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
+        //driver.findElement(By.name("user")).sendKeys("admin");
        // driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
+       // driver.findElement(By.name("pass")).sendKeys("secret");
        // driver.findElement(By.name("user")).click();
         //driver.findElement(By.name("pass")).click();
-        driver.findElement(By.xpath("//input[@value='LOGIN']")).click();// До этого использовалось Xpath выражение (.//*[normalize-space(text()) and normalize-space(.)='PASSWORD:'])[1]/following::input[2]
+        //driver.findElement(By.xpath("//input[@value='LOGIN']")).click();// До этого использовалось Xpath выражение (.//*[normalize-space(text()) and normalize-space(.)='PASSWORD:'])[1]/following::input[2]
+        driver.findElement(By.xpath("//a[contains(text(),'GROUPS')]")).click();
+        driver.findElement(By.xpath("//input[@value='NEW_GROUP']")).click();
+        driver.findElement(By.xpath(" //input[@name='group_name']")).sendKeys("Test111");
+        driver.findElement(By.xpath("//textarea[@name='group_header']")).sendKeys("Test111");
+        driver.findElement(By.xpath("//textarea[@name='group_footer']")).sendKeys("Test111");
+        driver.findElement(By.xpath("//input[@value='Enter information']")).click();
     }
 
     @AfterClass(alwaysRun = true)
